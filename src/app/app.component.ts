6967c2202a557cad4f4a8cecb975b52b685747b0
marcellodinizr <marcellodinizrocha@gmail.com>
2021-07-18 17:45:38 -0300

    general commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-setur';
+
+  openForm() {
+    const element: HTMLElement = document.getElementById('formulario')!;
+    element.setAttribute('style', 'display: block;');
+  }
 }
