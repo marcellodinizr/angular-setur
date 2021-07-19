489c432e8b331b5d29965bf87398cc41f4e752ad
marcellodinizr <marcellodinizrocha@gmail.com>
2021-07-19 10:55:44 -0300

    general commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-setur';
 
-  openForm() {
-    const element: HTMLElement = document.getElementById('formulario')!;
-    element.setAttribute('style', 'display: block;');
-  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { ContainerComponent } from './components/template/container/container.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, ContainerComponent],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
