2039ca02aa97200adb5da9d148a1587bcb7c32b5
marcellodinizr <marcellodinizrocha@gmail.com>
2021-07-19 11:54:16 -0300

    trying the fetch api with typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AuxilioService } from './auxilio.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,5 +10,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-setur';
 
-  
+  constructor(private auxilioService: AuxilioService) {}
+  fethAuxilio() {
+    this.auxilioService.fethAuxilio();
+  }
 }
