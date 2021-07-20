bb30b778287f729db0a6360849122221ecb38c62
marcellodinizr <marcellodinizrocha@gmail.com>
2021-07-20 11:49:36 -0300

    model for the api

---
+++ src/app/models/auxilio.model.ts
@@ -0,0 +1,14 @@
+export class Auxilios {
+  busca: [
+    {
+      nomepj: string;
+      nomeresponsavel: string;
+      razaosocial: string;
+      nomefantasia: string;
+      endereco: string;
+      email: string;
+      telefone: string;
+      municipio: string;
+    }
+  ];
+}
